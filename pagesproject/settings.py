f36907a6415e1669cbb9e89f8a39c94b378b2011
hansmd86 <hans.md.alcrosoft@gmail.com>
2022-02-25 07:36:51 +0000

    allowed hosts added

--- pagesproject/settings.py
+++ pagesproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3rh7ba-)9zibm**%rurc%*5x!mv32odt)ctlwsmm$5#!po#5@l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pagesproectapp.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
